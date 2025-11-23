5af45e4f7d5dfa341d8519d503513cfcbcc0f8ab
rummangaian <mohammad.r@mobiusdtaas.ai>
2025-11-23 20:06:36 +0530

    feat(coupons): add routes

---
+++ src/modules/coupons/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { CouponController } from './controllers/coupon.controller';
+import { CreateCouponSchema } from './dto/createCoupon.dto';
+import { ApplyCouponSchema } from './dto/applyCoupon.dto';
+import { ApplicableCouponsSchema } from './dto/applicableCoupons.dto';
+import { validateBody } from '../../common/middlewares/validationMiddleware';
+import { registerDefaultStrategies } from './strategies/strategyFactory';
+
+registerDefaultStrategies();
+
+const router = Router();
+const controller = new CouponController();
+
+router.post('/', validateBody(CreateCouponSchema), (req, res, next) => controller.create(req, res, next));
+router.get('/', (req, res, next) => controller.list(req, res, next));
+router.get('/:id', (req, res, next) => controller.get(req, res, next));
+router.post('/applicable', validateBody(ApplicableCouponsSchema), (req, res, next) => controller.applicable(req, res, next));
+router.post('/apply/:id', validateBody(ApplyCouponSchema), (req, res, next) => controller.apply(req, res, next));
+
+export default router;
