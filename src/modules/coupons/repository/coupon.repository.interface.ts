e20add51c7931c01b0171e672e007841b77c4786
rummangaian <mohammad.r@mobiusdtaas.ai>
2025-11-23 20:06:36 +0530

    feat(coupons): add repository interface

---
+++ src/modules/coupons/repository/coupon.repository.interface.ts
@@ -0,0 +1,9 @@
+import { CouponModel } from '../models/coupon.model';
+
+export interface ICouponRepository {
+  create(data: Partial<CouponModel>): Promise<CouponModel>;
+  findById(id: string): Promise<CouponModel | null>;
+  findAllActive(): Promise<CouponModel[]>;
+  update(id: string, data: Partial<CouponModel>): Promise<CouponModel>;
+  delete(id: string): Promise<void>;
+}
